6b43ab7173c0f48f98bb3b8c7d1cdcc96f090d90
Derek Miranda <derek.miranda@ff0000.com>
2017-12-05 11:41:08 -0800

    FrameRate and BAse

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+import Gesture from './lib/Gesture'
 import GestureEvent from './lib/GestureEvent'
 import GestureBase from './lib/GestureBase'
 import UIEvent from './lib/UIEvent'
-import Gesture from './lib/Gesture'
+import FrameRate from './lib/FrameRate'
+import FrameRateBase from './lib/FrameRateBase'
 
 export {
+	Gesture,
 	GestureEvent,
 	GestureBase,
 	UIEvent,
-	Gesture,
+	FrameRate,
+	FrameRateBase,
 }
