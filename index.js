e2e8b31a9e392b491f605f56f155fcb49d976f1a
Derek Miranda <derek.miranda@ff0000.com>
2017-12-05 09:53:14 -0800

    GestureBase

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import GestureEvent from './lib/GestureEvent'
+import GestureBase from './lib/GestureBase'
 import UIEvent from './lib/UIEvent'
 import Gesture from './lib/Gesture'
 
 export {
 	GestureEvent,
+	GestureBase,
 	UIEvent,
 	Gesture,
 }
